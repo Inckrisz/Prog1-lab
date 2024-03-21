dc103ea7b064da9f58c566e423e355c4ad52fca6
Inckrisz <inckrisz28@gmail.com>
2024-03-21 08:00:57 +0100

    Első commit

---
+++ elso/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("Írj be egy számot: ");
+    scanf("%d", &a);
+
+    for (int i = 0; i < a; ++i)
+    {
+        printf("Nev: Sára\n");
+    }
+}
